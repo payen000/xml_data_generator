b2d9f3426484953783a3d68f361b2b92417df34c
payen000 <payen@vauxoo.com>
2022-12-27 21:06:09 -0700

    [IMP] xml_data_generator: Added correct dependency arrangement

--- xml_data_generator/models/xml_data_generator.py
+++ xml_data_generator/models/xml_data_generator.py
@@ -1,16 +1,28 @@
-from os.path import dirname, join, exists
 from os import mkdir
+from os.path import dirname, exists, join
 
 from odoo import _, api, fields, models
-from odoo.exceptions import UserError, AccessError
+from odoo.exceptions import AccessError, UserError
 
 UNWANTED_FIELDS = {
+    "id",
     "create_uid",
     "create_date",
     "write_uid",
     "write_date",
     "__last_update",
-    "message_ids",
+}
+# "&" MUST go first to avoid ruining the escape of the other characters
+SPECIAL_CHARACTER_MAP = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+}
+TEXT_TTYPES = {
+    "char",
+    "text",
+    "html",
 }
 
 
@@ -24,7 +36,7 @@ class XmlDataGenerator(models.TransientModel):
         [("demo", "Export Anonymized Data"), ("real", "Export Real Data")],
         default="real",
         required=True,
-        help="Wether to anonimyze Char/Text fields or not."
+        help="Wether to anonimyze Char/Text fields or not.",
     )
     recursive_depth = fields.Integer(
         default=0,
@@ -55,8 +67,8 @@ class XmlDataGenerator(models.TransientModel):
             if not self.ignore_access:
                 raise AccessError(e)
             return {}
-        # Anonimyze record char/text data
-        if self.mode == "demo" and ttype in ["char", "text"] and current_value:
+        # Anonimyze record text data
+        if self.mode == "demo" and ttype in TEXT_TTYPES and current_value:
             current_value = "Demo %s" % field_name
             # If the target model has a method for anonimyzing the field, use it instead
             if hasattr(record, "_xml_data_generator_get_demo_%s" % field_name):
@@ -64,6 +76,11 @@ class XmlDataGenerator(models.TransientModel):
         # Exclude non-boolean False fields and fields that have the same value as their defaults
         if (not ttype == "boolean" and not current_value) or default_value == current_value:
             return {}
+        if ttype == "html" and not isinstance(current_value, str):
+            current_value = current_value.unescape()
+        if ttype in TEXT_TTYPES and current_value:
+            for char in SPECIAL_CHARACTER_MAP:
+                current_value = current_value.replace(char, SPECIAL_CHARACTER_MAP[char])
         return {field_name: {"value": current_value, "ttype": ttype}}
 
     def _prepare_xml_id(self, record_xid, table_name, id_):
@@ -78,7 +95,7 @@ class XmlDataGenerator(models.TransientModel):
             return "%s_demo_%s" % (table_name, id_)
         return "%s_%s" % (table_name, id_)
 
-    def _prepare_data_to_export(self, records, data, dependency_tree, model_data, recursive_depth):
+    def _prepare_data_to_export(self, records, data, dependency_tree, dependency_data, recursive_depth):
         if recursive_depth > self.recursive_depth:
             return data
         model_name = records._name
@@ -105,16 +122,21 @@ class XmlDataGenerator(models.TransientModel):
                 if ttype not in ["one2many", "many2one", "many2many"] or not field_values:
                     continue
                 related_recordset = field_values[field]["value"]
-                for r in related_recordset:
-                    child_xml_id = self._prepare_xml_id(r.get_external_id()[r.id], xml_model, r.id)
-                    child_model = r._name
+                for related_record in related_recordset:
+                    child_xml_id = self._prepare_xml_id(
+                        related_record.get_external_id()[related_record.id], xml_model, related_record.id
+                    )
+                    child_model = related_record._name
                     # Do not add one2many records to dependencies (only many2one and many2many)
                     if ttype != "one2many" and xml_id not in dependency_tree.get(child_xml_id, {}):
-                        dependency_tree.setdefault(xml_id, {}).update({child_xml_id: child_model})
-                    self._prepare_data_to_export(related_recordset, data, dependency_tree, model_data, recursive_depth + 1)
-            data[xml_id] = record_data
-            model_data.setdefault(model_name, {}).update({xml_id: dependency_tree.get(xml_id, {})})
-        return data, dependency_tree, model_data
+                        dependency_data["model_dependencies"].setdefault(model_name, set()).add(child_model)
+                        dependency_tree.setdefault(xml_id, set()).add(child_xml_id)
+                    self._prepare_data_to_export(
+                        related_recordset, data, dependency_tree, dependency_data, recursive_depth + 1
+                    )
+            data.setdefault(model_name, {}).update({xml_id: record_data})
+            dependency_data["record_dependencies"].update({xml_id: dependency_tree.get(xml_id, {})})
+        return data, dependency_data
 
     def _prepare_xml_row_to_append(self, field_name, dataset, dependency_tree):
         field_value = dataset[field_name]["value"]
@@ -160,9 +182,8 @@ class XmlDataGenerator(models.TransientModel):
         xml_records_code = []
         for xml_id, dataset in data.items():
             xml_code = []
-            db_id = dataset.pop("id")["value"]
             model_name = dataset["model_name"]
-            xml_model = dataset.pop("xml_model")
+            dataset.pop("xml_model")
             xml_code.append('    <record id="%s" model="%s">' % (xml_id, model_name))
             for field_name in dataset:
                 if field_name == "model_name":
@@ -174,109 +195,62 @@ class XmlDataGenerator(models.TransientModel):
             xml_records_code.append("\n".join(xml_code))
         return '<?xml version="1.0" ?>\n<odoo>\n%s\n</odoo>\n' % "\n\n".join(xml_records_code)
 
-    def _create_xml_data(self, xml_data_string):
+    def _create_xml_data(self, file_strings):
         # TODO: find a better way to find this path or maybe pass the path through args
         # this is asssuming we have the standard module -> models -> model.py structure
         module_path = dirname(dirname(__file__))
         data_dir = join(module_path, "xml_exported_data")
         if not exists(data_dir):
             mkdir(data_dir)
-        data_path = join(data_dir, "model_data.xml")
-        with open(data_path, "w") as xml_file:
-            xml_file.write(xml_data_string)
+        for model in file_strings:
+            model_name = model.replace(".", "_")
+            xml_data_string = file_strings[model]
+            data_path = join(data_dir, "%s.xml" % model_name)
+            with open(data_path, "w") as xml_file:
+                xml_file.write(xml_data_string)
 
-    {
-        'res.partner': {
-            'base.res_partner_address_15': {
-                'base.res_partner_12': 'res.partner', 
-                'base.state_us_5': 'res.country.state', 
-                'base.us': 'res.country'
-            }, 
-            'base.res_partner_address_28': {
-                'base.res_partner_12': 'res.partner', 
-                'base.state_us_5': 'res.country.state', 
-                'base.us': 'res.country'
-            }, 
-            'base.res_partner_address_16': {
-                'base.res_partner_12': 'res.partner', 
-                'base.state_us_5': 'res.country.state', 
-                'base.us': 'res.country'
-            }, 
-            'base.res_partner_12': {
-                'base.res_partner_category_11': 'res.partner.category', 
-                'base.state_us_5': 'res.country.state', 
-                'base.us': 'res.country'
-            }}
-        , 
-        'res.partner.category': {
-            'base.res_partner_category_11': {
+    def _resolve_dependency_tree(self, data2rearrange):
+        xml_id_list = list(data2rearrange.keys())
+        xml_id_list.reverse()
+        rearranged_data = {}
+        for xml_id in xml_id_list:
+            rearranged_data[xml_id] = data2rearrange.pop(xml_id)
+        return rearranged_data
 
-            }
-        }, 
-        'res.country.state': {
-            'base.state_us_5': {
-                'base.us': 'res.country'
-            }
-        }, 
-        'res.country': {
-            'base.us': {
-                'base.USD': 'res.currency'
-            }
-        }
-    }
-
-    def _probe_model(self, xml_ids, model, model_data, ordered_model_data):
-        for xml_id in xml_ids:
-            dependencies = xml_ids[xml_id]
-            for dependency in dependencies:
-                ordered_model_data.setdefault(model, [])
-                if dependency not in ordered_model_data[model]:
-                    if dependency in xml_ids:
-                        ordered_model_data[model] = [dependency] + ordered_model_data[model]
-                    else:
-                        ordered_model_data[model].append(dependency)
-                elif dependency not in xml_ids:
-                    counter_model = dependencies[dependency]
-                    counter_xml_ids = model_data.get(counter_model)
-                    if not counter_xml_ids:
-                        return
-                    self._probe_model(counter_xml_ids, counter_model, model_data, ordered_model_data)
-
-    def _resolve_dependency_tree(self, model_dependencies):
-        model_data_keys = list(model_dependencies)
-        model_data_keys.reverse()
-        ordered_model_data = {}
-        for model in model_data_keys:
-            xml_ids = model_dependencies[model]
-            self._probe_model(xml_ids, model, model_dependencies, ordered_model_data)
-        return ordered_model_data
+    def _show_xml_data(self, file_strings):
+        data2show = "\n".join([file_strings[model] for model in file_strings])
+        raise UserError(data2show)
 
     def action_export_to_xml(self):
         self.ensure_one()
         records2export = self._get_records_to_export()
-        data2export, dependency_tree, model_data = self._prepare_data_to_export(records2export, {}, {}, {}, 0)
-        resolved_list = self._resolve_dependency_tree(model_data)
-        data2export2 = {}
-        for model in resolved_list:
-            for xml_id in resolved_list[model]:
-                if data2export.get(xml_id):
-                    data2export2[xml_id] = data2export[xml_id]
-        breakpoint()
-        xml_data_string = self.prepare_xml_data_to_export(data2export2, dependency_tree)
+        data2export, dependency_data = self._prepare_data_to_export(
+            records2export,
+            {},
+            {},
+            {"record_dependencies": {}, "model_dependencies": {}},
+            0,
+        )
+        file_strings = {}
+        # TODO: use model_dependencies to suggest a dependency order
+        for model in dependency_data["model_dependencies"]:
+            model_data = self._resolve_dependency_tree(data2export[model])
+            xml_data_string = self.prepare_xml_data_to_export(model_data, dependency_data["record_dependencies"])
+            file_strings.update({model: xml_data_string})
         if self.show_data_as_error:
-            raise UserError(xml_data_string)
-        self._create_xml_data(xml_data_string)
+            self._show_xml_data(file_strings)
+        self._create_xml_data(file_strings)
 
     @api.onchange("recursive_depth")
     def _check_recursive_depth(self):
         if self.recursive_depth > 2:
             return {
-                'warning': {
-                    'title': _('Maximum recommended recursion level exceeded.'),
-                    'message': _(
-                        'Exceeding 2 recursion levels is not recommended, as record relations '
-                        'can grow rapidly without warning and the export operation could be '
-                        'really expensive. Proceed with caution or go back to level 2.'
-                    )
+                "warning": {
+                    "title": _("Maximum recommended recursion level exceeded."),
+                    "message": _(
+                        "Exceeding 2 recursion levels is not recommended, as record relations "
+                        "can grow rapidly without warning and the export operation could be "
+                        "really expensive. Proceed with caution or go back to level 2."
+                    ),
                 }
             }
