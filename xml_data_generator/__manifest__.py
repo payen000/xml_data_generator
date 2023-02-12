5ddf024014436211db4cba1df6ad6b1d51b58c00
payen000 <payen@vauxoo.com>
2023-02-11 20:16:08 -0700

    [ADD] xml_data_generator: Added demo data

--- xml_data_generator/__manifest__.py
+++ xml_data_generator/__manifest__.py
@@ -5,12 +5,15 @@
     "author": "Vauxoo",
     "website": "https://www.yourcompany.com",
     "category": "Uncategorized",
-    "version": "16.0.0.0.1",
+    "version": "14.0.0.0.1",
     "depends": ["base", "web"],
     "data": [
         "security/ir.model.access.csv",
         "wizard/xml_data_generator_views.xml",
         "views/assets.xml",
     ],
-    "demo": [],
+    "demo": [
+        "demo/res_partner.xml",
+        "demo/res_currency.xml",
+    ],
 }
